e63c92c9490ad4055fb278eae4d6ab5f3e134465
gitlinux12 <mshirole94@gmail.com>
2022-10-12 22:32:06 +0530

    initial

---
+++ RegexValidFirstName.java
@@ -0,0 +1,13 @@
+package userRegisteration;
+
+import java.util.regex.Pattern;
+
+public class RegexValidFirstName {
+	public static void  main(String[] args) {
+		String firstNameRegex = "^[A-Z]{1}[a-zA-z]{2,}$";
+		String FirstName = "Bridgelab";
+		boolean result = Pattern.matches(firstNameRegex, FirstName);
+		
+		System.out.println(result);
+	}
+}
