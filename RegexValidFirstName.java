43a1a661c9e75c6b94f1164ccb04a8f7b982dd9b
gitlinux12 <mshirole94@gmail.com>
2022-10-13 20:57:48 +0530

    First Name starts with Cap and has Minimum 3 char

--- RegexValidFirstName.java
+++ RegexValidFirstName.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class RegexValidFirstName {
 	public static void  main(String[] args) {
 		String firstNameRegex = "^[A-Z]{1}[a-zA-z]{2,}$";
-		String FirstName = "Bridgelab";
+		String FirstName = "Mayuri";
 		boolean result = Pattern.matches(firstNameRegex, FirstName);
 		
 		System.out.println(result);
